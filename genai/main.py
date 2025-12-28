87069e4fe5861ed0dac636070c04bf2906eda1b3
ujjwal-basnet <ujjwalvictus15@gmail.com>
2025-12-29 05:20:25 +0545

    func: added route for generating audio

--- genai/main.py
+++ genai/main.py
@@ -2,7 +2,11 @@ import uvicorn
 import uvicorn
 from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
-from models import load_text_model, generate_text ## resuse 
+from models import load_text_model, generate_text  , load_audio_model , generate_audio ## resuse 
+from schemas import VoicePresets
+from utils import audio_array_to_buffer
+from fastapi.responses import StreamingResponse
+
 app = FastAPI()
 
 @app.get("/", include_in_schema=False)
@@ -14,6 +18,24 @@ def serve_language_controller(prompt:str)-> str:
     pipe= load_text_model("cpu") 
     output= generate_text(pipe, prompt= prompt)
     return output
+
+
+@app.get(
+    "/generate/audio",
+    responses=  {status.HTTP_200_OK: {'content' : {'audio/wav': {}}}},
+    response_class = StreamingResponse,
+)
+def serve_text_to_audio_model_controller(
+    prompt: str,
+    preset: VoicePresets = "v2/en_speaker_1"):
+
+    process , model= load_audio_model()
+    output, sample_rate= generate_audio(process, model , prompt , preset)
+    return StreamingResponse(
+        audio_array_to_buffer(output, sample_rate) , media_type="audio/wav"
+    )
+
+
 
 # --- SERVER ---
 if __name__ == "__main__":
